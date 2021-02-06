1bf4d6645f7c6dbeababb9046e73d821bfdc0552
Ali Hosseini <ah.pb@web.de>
2021-02-06 15:00:51 +0100

    Figurenkopie

--- game.py
+++ game.py
@@ -1,6 +1,10 @@
 import turtle # Bibliothek turtle imortieren
 import PIL # Bibliothek pillow importieren 
 from PIL import Image # Funktion Image von Bibliothek pillow importieren
+import random
+
+yzombi = random.randint(-250,250) # y-Zufallposition
+speedzomb = random.randint(1.0,2.0)
 
 # Bild Gr0ße ändern
 mywidth=100 # gewunschte Bereite (100)
@@ -18,8 +22,16 @@ win.bgpic('h.gif') # Hintergrundbild
 # Bilder aus Festplatte importieren
 win.addshape('resize.gif')  # das geänderte Bild bringen wir in Python 
 
-# importierte Bilder als Figur nehmen
-zomb = turtle.Turtle()  # eine Figur bringen wir und zomb nennen wir sie
-zomb.shape('resize.gif') # das geäenderte Bild nehmen wir als Figur zomb ( Zombie )
+zombkopie = [] # eine Zombieliste erstellen
+
+# importierte Bilder als Figur und kopieren
+for i in range(3):
+    zomb = turtle.Turtle()  # eine Figur bringen wir und zomb nennen wir sie
+    zomb.shape('resize.gif') # das geäenderte Bild nehmen wir als Figur zomb ( Zombie )
+    zomb.penup()
+    zomb.speed(0)
+    zomb.setposition(350,(random.randint(-250,250)))
+    zomb.speed(random.randint(1.0,2.0))
+    zombkopie.append(zombkopie)
 
 turtle.done()
